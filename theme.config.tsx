91dbdfe2f0c6942f19e1da2561f7c5df59bfdc60
Matroid_Walker <adimensioner@gmail.com>
2024-09-18 15:26:10 +0800

    Update config

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,11 +2,11 @@
  * @Author: Matroid_Walker adimensioner@gmail.com
  * @Date: 2024-09-17 20:40:26
  * @LastEditors: Matroid_Walker adimensioner@gmail.com
- * @LastEditTime: 2024-09-17 21:03:33
+ * @LastEditTime: 2024-09-18 15:25:17
  * @FilePath: /dev/docs/knowledge_base/theme.config.tsx
- * @Description: 
- * 
- * Copyright (c) 2024 by Terry Chang, All Rights Reserved. 
+ * @Description:
+ *
+ * Copyright (c) 2024 by Terry Chang, All Rights Reserved.
  */
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
@@ -14,14 +14,14 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const config: DocsThemeConfig = {
   logo: <span>COMP 4211 notes</span>,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/TianxingChang/knowledge_base",
   },
   chat: {
     link: "https://discord.com",
   },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: "https://github.com/TianxingChang/knowledge_base",
   footer: {
-    text: "Nextra Docs Template",
+    text: "Dash Studio",
   },
 };
 
